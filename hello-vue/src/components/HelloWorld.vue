f561f343b611d389751f0ee3e26a30c8d87e2c66
SillyLord13 <hanna.ruoho@gmail.com>
2020-12-03 11:35:56 -0600

    heroku push

---
+++ hello-vue/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{helloMessage}}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data(){
+    return{
+      helloMessage: ''
+    }
+  },
+  props: {
+    msg: String
+  },
+  mounted() {
+    this.$hello_api.getHelloMessage().then(resp =>{
+      this.helloMessage = resp.message
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
